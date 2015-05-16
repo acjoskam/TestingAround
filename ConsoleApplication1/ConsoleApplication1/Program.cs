6283c3dda813f7d6ca46620b88b6ea92bd2b560a
Jaco Oskam <jaco.oskam@gmail.com>
2015-05-16 17:28:24 +0200

    Localized to NL

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             //simple test app
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hallo Wereld!");
             Console.ReadKey();
         }
     }
